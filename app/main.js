259cba2b920d0a384acbabfb31adc51c4ed32031
yogindra3926 <archief117@gmail.com>
2024-12-19 12:55:01 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -3,9 +3,23 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
- const server = net.createServer((connection) => {
+
+ const server = net.createServer((socket) => {
    // Handle connection
+   socket.on('data', (data) => {
+    const command = data.toString().trim();
+
+    let response = "Command not recognized";
+    if (command === 'PING') {
+      response = "+PONG\r";
+    } 
+
+    socket.write(response + '\n');
+  });
+
+  socket.on('error', (err) => {
+    console.error(err);
+  });
 });
 //
 server.listen(6379, "127.0.0.1");
