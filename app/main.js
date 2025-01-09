095a7ba1f0175782f37167a71aa5008fcf363e8a
yogindra3926 <archief117@gmail.com>
2025-01-09 20:30:04 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -58,8 +58,8 @@ if (addr.get("dir") && addr.get("dbfilename")) {
      let {m1,m2} = getKeysValues(rdb);
      map1=m1;
      y=m2;
-     console.log('Map3 (Key-ExpiryTime):', y);
-      console.log('Map2 (Key-Value):', map1);
+     console.log('Map3 (Key-ExpiryTime):', m1);
+      console.log('Map2 (Key-Value):', m2);
       console.log(`Successfully read RDB file: ${dbPath}`);
     } catch (error) {
       console.error(`Error reading DB at provided path: ${dbPath}`);
