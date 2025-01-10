4da23da2a689d699f2b6412ff9142f80a2aa2672
yogindra3926 <archief117@gmail.com>
2025-01-11 01:28:36 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -91,6 +91,98 @@ const master = net.createConnection({ host: masterArray[0], port: masterArray[1]
         // Send PSYNC ? -1
         sendCommand("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n", "+FULLRESYNC", () => {
           console.log("PSYNC acknowledged");
+          master.on("data", (data) => {
+            const command = Buffer.from(data).toString().split("\r\n");
+            if (command[2] === "PING") {
+              connection.write(serializeRESP("PONG"));
+            } else if (command[2] === "ECHO") {
+              const str = command[4];
+              connection.write(serializeRESP(str));
+            } else if (command[2] === "SET") {
+              broadcastToReplicas(data);
+              map1.set(command[4], command[6]);
+              if (command.length >= 8 && command[8] === "px") {
+                let interval = parseInt(command[10], 10);
+                let start = Date.now();
+                function accurateTimeout() {
+                  let elapsed = Date.now() - start;
+                  if (elapsed >= interval) {
+                    map1.delete(command[4]);
+                    console.log(`Key "${command[4]}" deleted after ${interval} ms`);
+                  } else {
+                    setTimeout(accurateTimeout, interval - elapsed);
+                  }
+                }
+                setTimeout(accurateTimeout, interval);
+              }
+              connection.write(serializeRESP(true));
+            } else if (command[2] === "GET") {
+              broadcastToReplicas(data);
+              console.log(`balle`);
+              let currentTimestamp = Date.now();
+              if (map1.has(command[4])){
+                console.log('Map3 (Key-ExpiryTime):', map3);
+               if(map3.has(command[4])){
+                console.log(`Key "${map3.get(command[4])}"`)
+                if(map3.get(command[4]) >= currentTimestamp){connection.write(serializeRESP(map1.get(command[4])));}
+                else{connection.write(serializeRESP(null));}
+               } 
+               else{connection.write(serializeRESP(map1.get(command[4])));}
+              } else {
+                connection.write(serializeRESP(null));
+              }
+            } else if (command[2] === "CONFIG" && command[4] === "GET") {
+              if (addr.has(command[6])) {
+                connection.write(
+                  serializeRESP([command[6], addr.get(command[6])])
+                );
+              } else {
+                connection.write(serializeRESP(null));
+              }
+            } else if (command[2] === "KEYS") {
+              broadcastToReplicas(data);
+             // Get all keys from map1
+          const keys = Array.from(map1.keys());
+          // Serialize keys into RESP format
+          const respKeys = `*${keys.length}\r\n` + keys.map(key => `$${key.length}\r\n${key}\r\n`).join('');
+          // Send the serialized response
+          connection.write(respKeys);
+            }else if (command[2] === "INFO") {
+              if (replicaidx !== -1) {
+                connection.write(serializeRESP("role:slave"));
+              } else {
+                // Construct the bulk string response for the INFO command
+                const info = [
+                  "role:master",
+                  "master_repl_offset:0",
+                  "master_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
+                ].join("\r\n"); // Combine lines with \r\n
+                
+                // Send the bulk string response
+                connection.write(`$${info.length}\r\n${info}\r\n`);
+              }
+            } else if (command[2] === "REPLCONF" && command[4] === "listening-port" && replicaidx ===-1) {
+              connection.write("+OK\r\n");
+            } else if (command[2] === "REPLCONF" && command[4] === "capa"  && replicaidx ===-1) {
+              connection.write("+OK\r\n");
+            } else if (command[2] === "PSYNC" && command[4] === "?" && command[6] === "-1"   && replicaidx ===-1) {
+              const clientAddress = `${connection.remoteAddress}:${connection.remotePort}`;
+              if (!replicaConnections.has(clientAddress)) {
+                  replicaConnections.set(clientAddress, connection);
+                  console.log(`Replica added: ${clientAddress}`);
+              }
+              const base64 = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="
+              const rdbBuffer = Buffer.from(base64, "base64");
+              const rdbHead = Buffer.from(`$${rdbBuffer.length}\r\n`)
+              
+           
+              connection.write("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n");
+              connection.write(Buffer.concat([rdbHead, rdbBuffer]));
+            }
+             else {
+              connection.write(serializeRESP("ERR unknown command"));
+            }
+          });
           master.end();
         });
       });
