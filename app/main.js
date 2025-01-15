a1842d21535c3c0d09f652c7db288b9e0afec375
yogindra3926 <archief117@gmail.com>
2025-01-15 23:05:52 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -413,6 +413,7 @@ connection.write(serializeRESP(res1));
     }else if(command[2].toUpperCase()=="XREAD"){
     let sizer=command.length;
     sizer-=5;
+    console.log(sizer);
       if(command[4].toLowerCase()=="block"){
 
       }
