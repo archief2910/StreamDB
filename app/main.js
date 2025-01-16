ab325e285302988f86cb2d568c9ad7623595bbf9
yogindra3926 <archief117@gmail.com>
2025-01-16 20:47:50 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -681,7 +681,7 @@ if(lastrange==command[10 + (2 * i) + (sizer / 2)]){res1.push(null);}
              else{
              if(isNaN(parseInt(map1.get(command[4])))){ans.push(map1.get(command[4]));} 
              else{console.log("gadbad yaha hain");
-              ans.push(`:${map1.get(command[4])}`);}
+              ans.push(`${map1.get(command[4])}`);}
              
               }
             } else {
